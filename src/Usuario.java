1f511cdb504386af6713bd18b7dad14b56592f0d
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-13 11:40:55 -0300

    enviando o projeto

---
+++ src/Usuario.java
@@ -0,0 +1,13 @@
+public class Usuario {
+
+    long cpf;
+    String nome;
+    String perfil;
+
+    public Usuario(long cpf, String nome, String perfil){
+        this.cpf = cpf;
+        this.nome = nome;
+        this.perfil = perfil;
+    }
+
+}
